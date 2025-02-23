0ea93e5c78aa4ba3314c17718a3f7f871dbd6b0e
Ternt <kyle.pham202@gmail.com>
2025-02-22 23:52:29 -0500

    organization and cleaning

--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-const port = 3000;
-
-app.listen(port, (err) => {
-    if (err) console.error("Something went wrong here.");
-    console.log(`Server listening at localhost:${port}`)
-});
