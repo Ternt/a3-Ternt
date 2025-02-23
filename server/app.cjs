d348352db7e9af5c23e2978ed6329865d4b01313
Ternt <kyle.pham202@gmail.com>
2025-02-23 07:33:06 -0500

    vercel json

--- server/app.cjs
+++ server/app.cjs
@@ -40,6 +40,25 @@ const initApp = async () => {
     return app;
 }
 
-module.exports = {
-    initApp: initApp,
-};
+const start = async () => {
+    if (!process.env.PORT ||
+        !process.env.HOST ||
+        !process.env.EXPRESS_SESSION_SECRET ||
+        !process.env.GITHUB_CLIENT_ID ||
+        !process.env.GITHUB_CLIENT_SECRET ||
+        !process.env.MONGO_USER ||
+        !process.env.MONGO_HOST ||
+        !process.env.MONGO_PASS
+    ) {
+        console.error("Please set app env variables");
+    }
+
+    const app = await initApp();
+    app.listen(process.env.PORT, () => {
+        console.log(`Server listening`);
+    });
+
+    return app;
+}
+
+module.exports = start();
